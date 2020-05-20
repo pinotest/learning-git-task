8f9f614a5aaa48d1c72572548cab6df2fe1870a3
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2020-05-20 19:45:32 +0200

    Add new dictionary and loop

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,2 +1,6 @@
 dict_shopping ={"warzywniak": ["cebula", "pomidory","zmieniaki"], "apteka":["witamina c", "nurofen"]}
 print("Lista zakupów")
+suma = 0
+for i in dict_shopping:
+    print(f"Idę do {i.capitalize()} i kupuję tam: {dict_shopping[i]}")
+    suma += len(dict_shopping[i])
